6e9ca144aadaa29d1ed076e221e9e4743d7871d4
Will Thames <wthames@redhat.com>
2016-04-04 11:32:31 +1000

    Improved utils, better standards handling

--- lib/ansiblereview/utils/__init__.py
+++ lib/ansiblereview/utils/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import print_function()
 from fabric.colors import red, green, yellow
+import re
 import sys
+from distutils.version import LooseVersion
 
 def abort(message):
     print(red("FATAL: %s" % message), file=sys.stderr)
@@ -17,3 +19,16 @@ def warn(message):
 
 def info(message):
     print(green("INFO: %s" % message), file=sys.stderr)
+
+
+def find_version(filename, version_regex="^# Standards: \([0-9]+(\.[0-9])+\)"):
+    with open(filename, 'r') as f:
+        for line in f:
+            match = version_regex.match(line)
+            if match:
+                return match.group(0)
+    return None
+
+
+def standards_latest(standards):
+    return max([standard.version for standard in standards], key=LooseVersion)
--- playbooks/fabfile.py
+++ playbooks/fabfile.py
@@ -1,9 +1,11 @@
 from fabric.api import task, local, env, settings
 from fabric.state import output
-from fabric.colors import red, green, yellow
+
+import ansiblereview.utils
+
 import sys
-from ansiblereview.utils import abort, error, info, \
-    warn, find_version, version_compare
+import optparse
+from distutils.version import LooseVersion
 
 
 env.colorize_errors = True
@@ -21,30 +23,44 @@ def syntax_check(playbook):
 
 @task
 def find_version(playbook):
-    version = find_version(playbook)
-    if not version:
-        error("Playbook %s does not present version. "
-              "Using latest standards" % playbook)
-        version = standards.LATEST_STANDARD
-    else:
-        info("Playbook %s declares version %s" % (playbook, version))
+    version = utils.find_version(playbook)
     return version
 
 
 @task(default=True)
-def review(playbook):
+def review(playbook, config):
     syntax_check(playbook)
-    standard_version = find_version(playbook)
-    for (version, standard) in standards.STANDARDS:
+    standard_version = utils.find_version(playbook)
+
+    sys.path.append(config.directory)
+    importlib.import_module('standards')
+
+    if not playbook_version:
+        playbook_version = utils.standards_latest(standards)
+        utils.error("Playbook %s does not present standards version. "
+              "Using latest standards version %s" %
+              (playbook, playbook_version))
+    else:
+        utils.info("Playbook %s declares standards version %s" %
+                (playbook, playbook_version))
+
+    for standard in standards:
+        if "playbook" not in standard.types:
+            continue
         result = standard.check(playbook)
         if result.failed:
-            if version_compare(version, standard_version) < 0:
-                warn("Future standard %s not met: %s" %
+            if LooseVersion(standard.version) < LooseVersion(playbook_version):
+                utils.warn("Future standard %s not met: %s" %
                      (standard.name, result.stderr))
             else:
-                error("Declared standard %s not met: %s" %
+                utils.error("Declared standard %s not met: %s" %
                       (standard.name, result.stderr))
 
 
 if __name__ == '__main__':
-    sys.exit(review(sys.argv[1]))
+    parser = optparse.OptionParser("%prog playbook.yml",
+                                    version="%prog " + __version__)
+    parser.add_option('-d', dest='directory',
+            help="Location of standards rules")
+    options, args = parser.parse_args(args)
+    sys.exit(review(args[1], options))
