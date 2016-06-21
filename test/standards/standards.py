289ce63a3e8c5f9efe7c47411a24757f15ba8c4d
Will Thames <wthames@redhat.com>
2016-06-21 10:17:39 +1000

    Update standards to use new types declaration
    types should be an array, not a comma separated string

--- test/standards/standards.py
+++ test/standards/standards.py
@@ -12,21 +12,22 @@ test_task_ansiblelint_success = Standard(dict(
     check = lintcheck('TEST0002'),
     name = "Test task lint success",
     version = "0.2",
-    types = "playbook,tasks"
+    types = ["playbook", "tasks", "handlers"]
 ))
 
 test_task_ansiblelint_failure = Standard(dict(
     check = lintcheck('TEST0001'),
     name = "Test task lint failure",
     version = "0.4",
-    types = "playbook,tasks"
+    types = ["playbook", "tasks", "handlers"]
 ))
 
 test_failure = Standard(dict(
     check = check_fail,
     name = "Test general failure",
     version = "0.5",
-    types = "playbook,tasks,vars"
+    types=["playbook", "task", "handler", "rolevars",
+           "hostvars", "groupvars", "meta"]
 ))
 
 test_success = Standard(dict(
