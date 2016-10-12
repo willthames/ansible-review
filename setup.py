5797119b42aac57a84a84bed8f3fc56b395fab93
JOHNSON, Matthew <matthew.johnson@suncorp.com.au>
2016-10-12 10:23:18 +1000

    Adding requirement for importlib

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     package_dir={'': 'lib'},
     packages=find_packages('lib'),
     zip_safe=False,
-    install_requires=['ansible-lint>=3.0.0', 'pyyaml', 'appdirs', 'unidiff', 'flake8'],
+    install_requires=['ansible-lint>=3.0.0', 'pyyaml', 'appdirs', 'unidiff', 'flake8', 'importlib'],
     scripts=['bin/ansible-review'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
